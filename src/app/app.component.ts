b14132f2aa0631aee43b7f81c924284c5eb3eab2
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2025-03-13 14:42:29 +0300

    creating and working with observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private destroy = inject(DestroyRef)
+  ngOnInit(): void {
+    const subscribe = interval(1000).subscribe({
+      next: (val) => { console.log(val) }, //triggered for every new value omitted
+      complete: () => { }, //would execute when the observable would not produce any more numbers
+      error: () => { } //would execute if an error occures
+    })
+
+    //always wanna unsubscrible to avoid memory leaks. 
+    this.destroy.onDestroy(() => {
+      subscribe.unsubscribe()
+    })
+  }
+
 
 }
