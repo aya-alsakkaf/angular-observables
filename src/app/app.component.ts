5f65d99258815be76c47bb402a19043b69e1a061
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2025-03-13 14:58:00 +0300

    siganls instead of observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { interval, map, pipe } from 'rxjs';
 
 @Component({
@@ -7,20 +7,40 @@ import { interval, map, pipe } from 'rxjs';
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-
+  clickCount = signal(0)
   private destroy = inject(DestroyRef)
-  ngOnInit(): void {
-    const subscribe = interval(1000).pipe(map((val) => val * 2)).subscribe({
-      next: (val) => { console.log(val) }, //triggered for every new value omitted
-      complete: () => { }, //would execute when the observable would not produce any more numbers
-      error: () => { } //would execute if an error occures
-    })
+  interval = signal(0)
+  doubleInterval = computed(() => this.interval() * 2)
 
-    //always wanna unsubscrible to avoid memory leaks. 
-    this.destroy.onDestroy(() => {
-      subscribe.unsubscribe()
+  constructor() {
+    effect(() => {
+      console.log("Clicked Button " + this.clickCount())
     })
   }
+  ngOnInit(): void {
+    // const subscribe = interval(1000).pipe(map((val) => val * 2)).subscribe({
+    //   next: (val) => { console.log(val) }, //triggered for every new value omitted
+    //   complete: () => { }, //would execute when the observable would not produce any more numbers
+    //   error: () => { } //would execute if an error occures
+    // })
 
+    // //always wanna unsubscrible to avoid memory leaks. 
+    // this.destroy.onDestroy(() => {
+    //   subscribe.unsubscribe()
+    // })
+
+
+    //-----signals
+
+    setInterval(() => {
+      this.interval.update((prevIntervalNumber) => prevIntervalNumber + 1)
+      console.log(this.doubleInterval())
+    }, 1000)
+  }
+
+
+  onClick() {
+    this.clickCount.update((prev) => prev + 1)
+  }
 
 }
